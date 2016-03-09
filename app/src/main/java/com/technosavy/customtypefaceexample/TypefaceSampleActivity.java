d10e776f566eca86d50679837bf393771cd66429
nayanesh <nayanesh.gupte@snapdeal.com>
2016-03-09 15:13:45 +0530

    first commit

---
+++ app/src/main/java/com/technosavy/customtypefaceexample/TypefaceSampleActivity.java
@@ -0,0 +1,13 @@
+package com.technosavy.customtypefaceexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TypefaceSampleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_typeface_sample);
+    }
+}
